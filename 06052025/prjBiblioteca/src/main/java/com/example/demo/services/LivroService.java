070ca35442984796c5b2770f9c53124db2b7bc89
JOHNVYTTOR <vittor.jsilva@gmail.com>
2025-05-06 09:08:17 -0300

    Add files via upload

---
+++ 06052025/prjBiblioteca/src/main/java/com/example/demo/services/LivroService.java
@@ -0,0 +1,29 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Livro;
+import com.example.demo.repositories.LivroRepository;
+
+@Service
+public class LivroService {
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    public Livro salvarLivro(Livro livro) {
+        return livroRepository.save(livro);
+    }
+
+    public Optional<Livro> buscarPorId(Long id) {
+        return livroRepository.findById(id);
+    }
+
+    public List<Livro> buscarTodos() {
+        return livroRepository.findAll();
+    }
+}
